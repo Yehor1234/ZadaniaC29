5494836ca7b3143357e4ffdfe3466ec2e4af4708
Więzień <153091248+Killergon@users.noreply.github.com>
2024-04-22 12:36:51 +0200

    Function wyświetlania tablicy

--- ZadC29/Program.cs
+++ ZadC29/Program.cs
@@ -1,7 +1,15 @@
-﻿class PlikGłówny
+﻿using ZadC29;
+
+class PlikGłówny
 {
     static void Main(string[] args)
     {
-        Tablice tablica = new Tablice();
-()    }
+    Tablice tablica = new Tablice();
+        int[] tab = tablica.Tworzenie_tablicy();
+
+        for (int i = 0; i < tab.Length; i++)
+        {
+            Console.WriteLine(tab[i]);
+        }
+    }
 }
--- ZadC29/Tablice.cs
+++ ZadC29/Tablice.cs
@@ -8,5 +8,25 @@ namespace ZadC29
 {
     public class Tablice
     {
+        public void Wyswietl(int[] tab)
+        {
+            for(int i = 0; i < tab.Length; i++)
+            {
+                Console.WriteLine(tab[i] + ",");
+            }
+           
+        }
+        public int[] Tworzenie_tablicy()
+        {
+            Console.WriteLine("ile znaków ma mieć tablica");
+            int dl = int.Parse(Console.ReadLine());
+            int[] ints = new int[dl];
+            for (int i = 0; i < dl; i++)
+            {
+                Console.Write("podaj kolejny element");
+                ints[i] = int.Parse(Console.ReadLine()) ;
+            }
+            return ints;
+        }
     }
 }
